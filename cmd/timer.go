1ae1345bc72d1478163e4f32057f013da5ea845f
MeztliRA <MeztliRA@gmail.com>
2021-03-30 16:12:38 +0700

    Add gemitTimer() function in timer.go

--- cmd/timer.go
+++ cmd/timer.go
@@ -2,8 +2,15 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	timerStartedMessage = "timer started!"
+	timerDoneMessage    = "timer done!"
 )
 
 // timerCmd represents the timer command
@@ -13,7 +20,7 @@ var timerCmd = &cobra.Command{
 	Long: `Start a new gemit timer of a specified duration.
 	the duration used for the timer is from the argument passed.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("timer called")
+		gemitTimer(args)
 	},
 }
 
@@ -29,4 +36,16 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// timerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func gemitTimer(args []string) {
+	inputtedDuration := args[0]
+	duration, err := time.ParseDuration(inputtedDuration)
+	if err != nil {
+		log.Fatal(err)
+	}
+	timer := time.NewTimer(duration)
+	fmt.Println(timerStartedMessage)
+	<-timer.C
+	fmt.Println(timerDoneMessage)
 }
