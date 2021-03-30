ab6fae2298d52a3ddeac0d5162d31f01d2085798
MeztliRA <MeztliRA@gmail.com>
2021-03-30 16:04:31 +0700

    Add description in timer.go

--- cmd/timer.go
+++ cmd/timer.go
@@ -10,12 +10,8 @@ import (
 var timerCmd = &cobra.Command{
 	Use:   "timer",
 	Short: "Start a new timer",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Start a new gemit timer of a specified duration.
+	the duration used for the timer is from the argument passed.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("timer called")
 	},
