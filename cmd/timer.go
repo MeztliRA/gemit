c659f2ebac30e6809c9c460b03589fa47f389d62
MeztliRA <MeztliRA@gmail.com>
2021-04-01 14:31:17 +0700

    Removed alot of comments generated by cobra init

--- cmd/timer.go
+++ cmd/timer.go
@@ -28,16 +28,6 @@ var timerCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(timerCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// timerCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// timerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func gemitTimer(args []string) {
